54d06c568ef3960f41983a67b92fba5f5955dc7b
Panshul <panshulsingh2005@gmail.com>
2024-09-04 20:18:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Forum from "./pages/Forum.jsx";
+import Directory from "./pages/Directory";
+import Donation from "./pages/Donation";
+import Jobs from "./pages/Jobs";
+import Navbar from "./components/navbar/Navbar.jsx";
+import NavbarBoot from "./components/navbar/Navbar.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <NavbarBoot/>
+      <Routes>
+        <Route path="/directory" element={<Directory />} />
+        <Route path="/donation" element={<Donation />} />
+        <Route path="/job" element={<Jobs />} />
+        <Route path="/forum" element={<Forum />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
